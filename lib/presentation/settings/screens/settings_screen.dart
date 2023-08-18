cc2edef3dbb6b9d5b7337213a90c98fe3fd9ceeb
potikorn.t <potikorn.t@20scoops.net>
2023-08-18 17:35:57 +0700

    feat: add language switcher for testing

--- lib/presentation/settings/screens/settings_screen.dart
+++ lib/presentation/settings/screens/settings_screen.dart
@@ -1,8 +1,11 @@
 // Flutter imports:
+
+// Flutter imports:
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:easy_localization/easy_localization.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
@@ -23,6 +26,13 @@ class SettingScreen extends StatelessWidget {
             .watch(authStateNotifierProvider.select((value) => value.loading));
         return Column(
           children: [
+            ListTile(
+              title: Text("language".tr()),
+              trailing: Text(context.locale.languageCode.toUpperCase()),
+              onTap: () {
+                Navigator.of(context).restorablePush(dialogBuilder);
+              },
+            ),
             ElevatedButton(
               onPressed: () async {
                 clearProviders(ref);
@@ -37,4 +47,40 @@ class SettingScreen extends StatelessWidget {
       },
     );
   }
+}
+
+Route<Object?> dialogBuilder(BuildContext context, Object? arguments) {
+  return DialogRoute<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        icon: const Icon(Icons.language),
+        title: const Text('Basic dialog title'),
+        content: Column(
+          children: context.supportedLocales
+              .map(
+                (e) => ListTile(
+                  title: Text(e.languageCode.toUpperCase()),
+                  onTap: () {
+                    context.setLocale(e);
+                    context.pop();
+                  },
+                ),
+              )
+              .toList(),
+        ),
+        actions: [
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text('cancel'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
